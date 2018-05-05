317dc674d83937d1c27193df69e1a55bdca9af2a
LT <Tony-Liou@users.noreply.github.com>
2018-05-05 17:16:04 +0800

    Code comment and readme file

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # ML2018_410421209
+- [Assignment1](#image-decryption-by-a-single-layer-neural-network)
+
+
 
 ## Image decryption by a single-layer neural network
+
+**Table of contents**
+---------------------
+- [Training samples](#training-samples)
+- [Parameters](#parameters)
+- [Weight vector](#weight-vector)
+- [Display of images](#display-of-images)
+- [Problems](#problems)
+- [Learning experience](#learning-experience)
 
 ### Training samples
 Use the data from e-Learning
@@ -11,14 +23,19 @@ Use the data from e-Learning
 - α: 1e-7
 - 𝜖: 1e-30
 
-### Weight vector W
+### Weight vector
 
 |  |w1|w2|w3|
 |--|--|--|--|
-|0.25006898|0.65998043|0.0901495|
+|value|0.25006898|0.65998043|0.0901495|
 
-### Printed image I' decrypted from E'
+### Display of images
+
+- *Incrypted image*
+![Original image](Image_and_ImageData/Eprime.png)
+- *Decrypted image*
 ![Original image](Image_and_ImageData/origin.png)
+
 ### Problems
 
 ### Learning experience
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-import os
 
 # Read images
 E = Image.open("Image_and_ImageData/E.png")
@@ -13,7 +12,7 @@ Eprime = Image.open("Image_and_ImageData/Eprime.png")
 width, height = E.size
 output = Image.new("L", (width, height), 0)
 
-# Convert images to number array
+# Convert images to numpy array
 arrE = np.array(E)
 arrK1 = np.array(key1)
 arrK2 = np.array(key2)
@@ -24,26 +23,21 @@ w = np.zeros(3) # [0., 0., 0.] randomize
 max = 10 # Max iteration limit
 epsilon = 1e-30 # vigilance level
 alpha = 1e-7 # Learning rate
-epoch = 1
+epoch = 1 # time(s)
 
+# Training algorithm
 while True:
     preW = w
     for i in range(0, height-1):
         for j in range(0, width-1):
             x = np.array([arrK1[i][j], arrK2[i][j], arrI[i][j]])
-            a = w.dot(x.T)
+            a = w.dot(x.T) # dot is matrix multiplication, T is transpose
             e = arrE[i][j] - a
             w = w + (alpha * e) * x
-            #print(a)
-        #alpha /= 10
 
     if epoch >= max or abs(np.linalg.norm(w - preW)) <= epsilon:
         break
     epoch += 1
 
-output = (Eprime - (w[0] * key1) - (w[1] * key2)) / w[2]
-Image.fromarray(output).show()
-
-print("MaxIterLimit: ", max, "alpha: %.20f"%alpha, "epsilon: ", epsilon)
-print("W = ", w)
-os.system("pause")
+output = (Eprime - (w[0] * key1) - (w[1] * key2)) / w[2] # '*' is also matrix multiplication
+Image.fromarray(output).show() # Display the final image
