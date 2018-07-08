4ac6ae1fcb4da083089dc4af28e15ac4c91d9b70
Liou, Guan-Cheng <Tony-Liou@users.noreply.github.com>
2018-07-08 22:52:18 +0800

    Fixed code

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,47 +1,43 @@
+from sklearn.datasets import fetch_mldata
 import numpy as np
+import matplotlib.pyplot as plt #plotting package
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-#设置 PCA 降纬后的纬度值
+import time
+
+# Setting PCA dimension value after reducing dimension
 COMPONENT_NUM = 50
 
-print('Read training data...')
 
-#读取训练数据
-with open('train.csv','r') as read:
-    #读取文件第一行的表头，不做处理
-    read.readline()
-    #训练文件的标签集
-    train_label = []
-    #训练文件的数据集
-    train_data = []
-    for line in read.readlines():
-        #map()是 Python 内置的高阶函数，它接收一个函数 f 和一个 list，
-        #并通过把函数 f 依次作用在 list 的每个元素上，得到一个新的 list 并返回
-        #把 list 里的所有元素转换为 整型 返回
-        data = list( map(int,line.rstrip().split(',')))
-        #把 data 里的第一个元素加入到标签集
-        train_label.append(data[0])
-        #把 data 里剩余的元素加入到数据集
-        train_data.append(data[1:])
+DATA_PATH = '~/scikit_learn_data'
 
-print('Loaded ' + str(len(train_label)))
+print("Fetch training data...")
+mnist = fetch_mldata('MNIST original', data_home=DATA_PATH) # https://github.com/amplab/datascience-sp14/blob/master/lab7/mldata/mnist-original.mat
+print("Fetch finished")
+
+img = np.reshape(mnist.data[0,:], (28,28)) # reshape 1-D vector of the first example to a 2-D image, first row of the array
+plt.imshow(img, cmap=plt.cm.gray ) # prepare image for display
+plt.show() # display image
+
+mnist.data = mnist.data / 255
 
 print('Reduction...')
 
-# 将 list 转换为 numpy 数组
-train_label = np.array(train_label)
-train_data = np.array(train_data)
-
-# 原始数据集的维度
-print (train_data.shape)
+# Original dimension
+print (mnist.data.shape)
 pca = PCA(n_components=COMPONENT_NUM, whiten=True)
 # Fit the model with X
-pca.fit(train_data)
-#对数据集 train_data 进行降纬
-train_data = pca.transform(train_data)
-#降纬后的数据集纬度
-print (train_data.shape )
+pca.fit(mnist.data)
+# Reducing dimension
+X = pca.transform(mnist.data)
+# Show reduced dimension
+print (X.shape)
+Y = mnist.target
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)
+
 
 print('Train SVM...')
 
@@ -51,39 +47,25 @@ print('Train SVM...')
 # 支持向量机 SVM
 svc = SVC()
 #训练 SVM
-svc.fit(train_data, train_label)
+tStart = time.clock() # Timer start
+svc.fit(x_train, y_train)
+tEnd = time.clock() # Timer stop
+print("Training costs:", tEnd - tStart, "sec")
 
-print('Read testing data...')
-
-#加载测试集
-with open('test.csv','r') as read:
-    #读取文件第一行的表头，不做处理
-    read.readline()
-    #测试数据集
-    test_data = []
-    for line in read.readlines():
-        #map 把 list 里的所有元素转换为 整型 返回
-        data = list(map(int, line.rstrip().split(',')))
-        #把所有数据加入测试数据集
-        test_data.append(data)
-
-print('Loaded ' + str(len(test_data)))
 
 print('Predicting...')
-#将 list 转换为 numpy 数组
-test_data = np.array(test_data)
-#降纬
-test_data = pca.transform(test_data)
-#测试结果
-predict = svc.predict(test_data)
+tStart = time.clock() # Timer start
+predict = svc.predict(x_test)
+tEnd = time.clock() # Timer stop
+print("Predicting costs:", tEnd - tStart, "sec")
 
-print('Saving...')
 
-#将测试结果保存到 sample_submission.csv
-with open('sample_submission.csv', 'w') as writer:
-    #第一行写入表头
-    writer.write('"ImageId","Label"\n')
-    count = 0
-    for p in predict:
-        count += 1
-        writer.write(str(count) + ',"' + str(p) + '"\n')
+right = 0
+wrong = 0
+for i in range(len(predict)):
+    if y_test[i] == predict[i]:
+        right += 1
+    else:
+        wrong += 1
+
+print("Right:", right, ", wrong:", wrong)
