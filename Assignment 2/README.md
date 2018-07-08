3365b5609efd20fbdbb18e67592942842eff4524
Liou, Guan-Cheng <Tony-Liou@users.noreply.github.com>
2018-07-09 00:03:28 +0800

    Small update

--- Assignment 2/README.md
+++ Assignment 2/README.md
@@ -1,1 +1,22 @@
 # Handwritten digit recognition
+
+## Implementation
+
+1. 從網路擷取數據
+1. 數值標準化
+1. 降低維度
+1. 分類
+1. 測試辨識準確度
+
+## Functions
+
+使用scikit-learn作為主要的package
+- fetch_mldata
+- train_test_split
+- PCA(Principal Component Analysis)
+- SVC(Support Vector Machine)
+
+## Problems
+
+一開始使用fetch_mldata，一直出現WinError 10060無法取得資料，最後只好上網搜尋別人已經下載好的
+在調整降維數值也花了一段時間，雖然看不太出變化，最後決定設為50，有可能是因為降維跟訓練的演算法用的太少，所以比較不出來
--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import fetch_mldata
-import numpy as np
-import matplotlib.pyplot as plt #plotting package
+'''import numpy as np
+import matplotlib.pyplot as plt #plotting package'''
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -17,46 +17,32 @@ print("Fetch training data...")
 mnist = fetch_mldata('MNIST original', data_home=DATA_PATH) # https://github.com/amplab/datascience-sp14/blob/master/lab7/mldata/mnist-original.mat
 print("Fetch finished")
 
+'''
 img = np.reshape(mnist.data[0,:], (28,28)) # reshape 1-D vector of the first example to a 2-D image, first row of the array
 plt.imshow(img, cmap=plt.cm.gray ) # prepare image for display
 plt.show() # display image
+'''
 
-mnist.data = mnist.data / 255
+mnist.data = mnist.data / 255 # Normalized
 
-print('Reduction...')
-
-# Original dimension
-print (mnist.data.shape)
 pca = PCA(n_components=COMPONENT_NUM, whiten=True)
 # Fit the model with X
 pca.fit(mnist.data)
 # Reducing dimension
 X = pca.transform(mnist.data)
-# Show reduced dimension
-print (X.shape)
 Y = mnist.target
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)
 
-
-print('Train SVM...')
-
-#拟合模型
-#KNN = KNeighborsClassifier() 
-# 逻辑回归：model = LogisticRegression()
-# 支持向量机 SVM
 svc = SVC()
-#训练 SVM
 tStart = time.clock() # Timer start
 svc.fit(x_train, y_train)
 tEnd = time.clock() # Timer stop
 print("Training costs:", tEnd - tStart, "sec")
 
-
-print('Predicting...')
-tStart = time.clock() # Timer start
+tStart = time.clock()
 predict = svc.predict(x_test)
-tEnd = time.clock() # Timer stop
+tEnd = time.clock()
 print("Predicting costs:", tEnd - tStart, "sec")
 
 
